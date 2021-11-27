44193a0774a620cf530880b66952300a721112e6
Dakota Castle <barrettcastle@gmail.com>
2021-11-27 13:14:15 -0500

    commit tree f3c59ea6603853196ff398032fbd8c5cad824224

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2)
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = x => {
+    return function(x) {
+        return x * x
+    }
+}
+
+const fareDoubler = m => m * 2;
+
+const fareTripler = m => m * 3;
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+}
